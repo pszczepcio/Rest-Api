397778e3799be6820a0db3254855662ca67e08cf
golfik2005 <spiotrek1985@gmail.com>
2019-03-13 23:51:21 +0100

    Task 19.2

---
+++ src/main/java/com/crud/tasks/com/crud/tasks/service/DbService.java
@@ -0,0 +1,26 @@
+package com.crud.tasks.com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DbService {
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> getAllTasks(){
+        return repository.findAll();
+    }
+
+    public Task getTask(Long id) {
+        Task task= new Task();
+        if(repository.findById(id) != null){
+            task = repository.findById(id);
+        }
+        return task;
+    }
+}
