f2e050c63ec7b9d8c4471bc4c26ff1fb379f621a
golfik2005 <spiotrek1985@gmail.com>
2019-03-28 12:22:00 +0100

    Task 23.2

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,6 +1,5 @@
 package com.crud.tasks;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,8 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TasksApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 }
